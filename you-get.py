d1373ee0616b7abd0bb45112b29382baca5190bb
xuan's lab <xuan1024chem@gmail.com>
2024-10-26 14:23:59 +0800

    Update you-get.py

--- you-get.py
+++ you-get.py
@@ -1,14 +1,25 @@
-import os, sys
+import os
+import sys
 
-_srcdir = '%s/src/' % os.path.dirname(os.path.realpath(__file__))
-_filepath = os.path.dirname(sys.argv[0])
-sys.path.insert(1, os.path.join(_filepath, _srcdir))
+def add_src_to_path():
+    """Add the src directory to the system path."""
+    # 获取当前脚本的目录并构建源代码路径
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    src_dir = os.path.join(script_dir, 'src')
+    sys.path.insert(1, src_dir)
 
-if sys.version_info[0] == 3:
-    import you_get
-    if __name__ == '__main__':
-        you_get.main(repo_path=_filepath)
-else: # Python 2
-    from you_get.util import log
-    log.e("[fatal] Python 3 is required!")
-    log.wtf("try to run this script using 'python3 you-get'.")
+def main():
+    """Main function to execute the you-get script."""
+    add_src_to_path()
+    
+    # 检查Python版本并导入you-get模块
+    if sys.version_info[0] == 3:
+        import you_get
+        you_get.main(repo_path=os.path.dirname(sys.argv[0]))
+    else:  # 处理Python 2
+        from you_get.util import log
+        log.e("[fatal] Python 3 is required!")
+        log.wtf("Please run this script using 'python3 you-get'.")
+
+if __name__ == '__main__':
+    main()
